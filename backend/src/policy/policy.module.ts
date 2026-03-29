e7adaa4ee1e68598e38d222a1be41a234fad33c9
FlexCanCode <flexydew@gmail.com>
2026-03-29 03:04:03 +0100

    feat: addd scheduled BullMQ-based policy renewal reminder job

--- backend/src/policy/policy.module.ts
+++ backend/src/policy/policy.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
-import { PolicyController } from './policy.controller';
-import { PolicyService } from './policy.service';
-import { RenewalController } from './renewal.controller';
-import { RenewalService } from './renewal.service';
-import { RpcModule } from '../rpc/rpc.module';
+import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
+import { PolicyController } from "./policy.controller";
+import { PolicyService } from "./policy.service";
+import { RenewalController } from "./renewal.controller";
+import { RenewalService } from "./renewal.service";
+import { RenewalReminderService } from "./renewal-reminder.service";
+import { RpcModule } from "../rpc/rpc.module";
+import { NotificationsModule } from "../notifications/notifications.module";
 
 @Module({
-  imports: [RpcModule],
+  imports: [ScheduleModule.forRoot(), RpcModule, NotificationsModule],
   controllers: [PolicyController, RenewalController],
-  providers: [PolicyService, RenewalService],
+  providers: [PolicyService, RenewalService, RenewalReminderService],
   exports: [RenewalService],
 })
 export class PolicyModule {}
